214936546caf0dfa3c162e6ff98283d249afb796
Amit Raban <amit.raban@gmail.com>
2024-01-23 20:19:04 +0200

    fix import

--- sdxl_the_chosen_one.py
+++ sdxl_the_chosen_one.py
@@ -50,7 +50,8 @@ from diffusers.models.lora import LoRALinearLayer
 from diffusers.optimization import get_scheduler
 from diffusers.training_utils import compute_snr
 from diffusers.utils import check_min_version, is_wandb_available
-from diffusers.utils.import_utils import is_xformers_available, is_torch_bf16_gpu_available
+from diffusers.utils.import_utils import is_xformers_available
+from transformers.utils.import_utils import is_torch_bf16_gpu_available
 from PIL import Image
 import PIL
 import safetensors
